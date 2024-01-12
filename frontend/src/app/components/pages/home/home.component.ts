1cade3b820b031c1288e8067f71f66882a700e30
Mezen <mezenbenkemil@gmail.com>
2024-01-12 15:49:23 +0100

    Projet terminé

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Food } from '../../../shared/models/Food';
+import { FoodService } from '../../../services/food.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements OnInit {
+  foods: Food[] = [];
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute
+  ) {
+    let foodsObservalbe: Observable<Food[]>;
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        foodsObservalbe = this.foodService.getAllFoodsBySearchTerm(
+          params.searchTerm
+        );
+      else if (params.tag)
+        foodsObservalbe = this.foodService.getAllFoodsByTag(params.tag);
+      else foodsObservalbe = foodService.getAll();
+
+      foodsObservalbe.subscribe((serverFoods) => {
+        this.foods = serverFoods;
+      });
+    });
+  }
+  ngOnInit(): void {}
+}
---
+++ frontend/src/app/shared/models/Food.ts
@@ -0,0 +1,11 @@
+export class Food {
+  id!: string;
+  name!: string;
+  price!: number;
+  tags?: string[];
+  favorite!: boolean;
+  imageUrl!: string;
+  stars!: number;
+  origins!: string[];
+  cookTime!: string;
+}
